c972d27074e8a399dcc14c835cd63ebc3a27fb2e
Elton Lee <elton2048@gmail.com>
2019-11-25 12:59:35 +0800

    Provide catch error in signin function for showing in-app browser
    In `signin` function, the `catch` function is required as the closing
    action returns error. Missing the `catch` function from it will return
    error in client.
    
    Minor changes:
    
     - Provide command to have iOS 11 in testing
     - Provide sign in using async await function
    
    Resolves: #21

--- example/src/HomeScreen.js
+++ example/src/HomeScreen.js
@@ -12,10 +12,12 @@ export default class App extends Component {
   constructor (props) {
     super(props);
     const authcore = new Authcore({
-      baseUrl: 'http://localhost:8000'
+      // baseUrl: 'http://localhost:8000'
       // baseUrl: 'http://10.0.2.2:8000'
+      baseUrl: 'https://authcore.dev:8001'
+      // baseUrl: 'https://authcore-staging.blocksq.com'
     });
-    this.state = {authcore: authcore};
+    this.state = {authcore: authcore}
   }
 
   componentDidMount() {
@@ -27,7 +29,20 @@ export default class App extends Component {
       this.setState({
         accessToken: data.accessToken
       })
-    });
+    })
+      .catch(err => console.log(err))
+  }
+
+  async _onLoginAsync () {
+    try {
+      const { accessToken } = await this.state.authcore.webAuth.signin()
+      // Access token should be stored in keychain for security
+      this.setState({
+        accessToken: data.accessToken
+      })
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   render() {
@@ -38,6 +53,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>☆Authcore example☆</Text>
         <Button onPress = { this._onLogin } title={ 'Sign In' } />
+        <Button onPress = { this._onLoginAsync.bind(this) } title={ 'Sign In Async' } />
         <Text>{this.state.accessToken}</Text>
         <Button onPress={() => navigate('ProfileScreen', {
             accessToken: this.state.accessToken,
